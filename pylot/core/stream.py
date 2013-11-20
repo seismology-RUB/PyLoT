bf37a6546de8a5a355ffb86c1c6d6ef3e6294988
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2013-11-20 06:02:43 +0100

    Started to build the basic structure. Most classes will inherit functionality of the ObsPy Core classes, but slightly redefined to fit PyLoTs’ purposes.

---
+++ pylot/core/stream.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 15:13:13 2013
+
+@author: sebastianw
+"""
+
+from obspy.core import Stream as Obspystream
+
+
+class Stream(Obspystream):
+    pass
+
+
+def read():
+    pass
