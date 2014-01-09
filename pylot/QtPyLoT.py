e05c3d56bcba0e031d0479ad136637642f4ccfa4
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2014-01-09 10:43:40 +0100

    initialized software versioning similar to obspy

---
+++ pylot/QtPyLoT.py
@@ -0,0 +1,53 @@
+# Main program: QtPyLoT.py
+#
+# 
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+import os
+import platform
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import helpform
+from pylot.core.util import _getVersionString
+
+# Version information
+__version__ = _getVersionString()
+
+# Creating a Qt application
+pylot_app = QApplication(sys.argv)
+
+pylot_main = QWidget()
+pylot_main.setWindowTitle('TestWindow')
+
+ok_btn = QPushButton('OK', pylot_main)
+
+@pyqtSlot()
+def on_click():
+	print('clicked')
+
+@pyqtSlot()
+def on_press():
+	print('pressed')
+
+@pyqtSlot()
+def on_release():
+	print('released')
+
+# Connect signals to slots
+ok_btn.clicked.connect(on_click)
+ok_btn.pressed.connect(on_press)
+ok_btn.pressed.connect(on_release)
+
+# Show window and run the app
+pylot_main.show()
+pylot_app.exec_()
---
+++ pylot/core/util/__init__.py
@@ -0,0 +1,1 @@
+from pylot.core.util.version import get_git_version as _getVersionString
