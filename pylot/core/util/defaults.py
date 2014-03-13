84f3a29a8638420fbf21106e174789c67710ca46
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2014-03-13 09:24:17 +0100

    new file defaults.py written to provide default value if no user configuration file is available

---
+++ pylot/core/util/defaults.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 26 12:31:25 2014
+
+@author: sebastianw
+"""
+
+FILTERDEFAULTS = {'P': {'filtertype': None,
+                        'order': None,
+                        'freq': None},
+                  'S': {'filtertype': 'bandpass',
+                        'order': '4',
+                        'freq': [.5, 5]}}
