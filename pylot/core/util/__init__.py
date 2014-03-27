dac3be5110480c01e1f05c4e01bcc851d6c029aa
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2014-03-27 21:29:41 +0100

    package wide imports for convenience

--- pylot/core/util/__init__.py
+++ pylot/core/util/__init__.py
@@ -1,3 +1,4 @@
 from pylot.core.util.version import get_git_version as _getVersionString
 from pylot.core.util.defaults import FILTERDEFAULTS
 from pylot.core.util.widgets import *
+from pylot.core.util.errors import *
