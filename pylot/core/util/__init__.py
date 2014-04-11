d9ca0141de91d13ffc87bd48c9bdaa5b3ca02d64
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2014-04-11 19:40:11 +0200

    alphabetic order of convenience imports

--- pylot/core/util/__init__.py
+++ pylot/core/util/__init__.py
@@ -1,6 +1,6 @@
-from pylot.core.util.version import get_git_version as _getVersionString
+from pylot.core.util.connection import checkurl
 from pylot.core.util.defaults import FILTERDEFAULTS
+from pylot.core.util.errors import OptionsError
+from pylot.core.util.version import get_git_version as _getVersionString
 from pylot.core.util.widgets import (PickDlg, MPLWidget, PropertiesDlg,
                                      FilterOptionsDock)
-from pylot.core.util.errors import OptionsError
-from pylot.core.util.connection import checkurl
