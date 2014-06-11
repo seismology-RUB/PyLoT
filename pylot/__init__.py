4c8174f5aecc2ec6233561936c986f5d0827534c
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2014-06-11 15:20:55 +0200

    convenience imports completed

--- pylot/__init__.py
+++ pylot/__init__.py
@@ -28,5 +28,5 @@ The development of PyLoT is part of the joint research project MAGS2.
 
 from obspy.core.utcdatetime import UTCDateTime
 from obspy.core.util.attribdict import AttribDict
-#from pylot.core.trace import Stats, Trace
-#from pylot.core.stream import Stream, read
+from pylot.core.read import *
+from pylot.core.util import *
