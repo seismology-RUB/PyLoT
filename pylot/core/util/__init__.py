898169647bd473cb9744da76cbd0b2e75f24c717
Sebastian Wehling <sebastianw@minos02.geophysik.ruhr-uni-bochum.de>
2014-07-25 14:15:06 +0200

    added new convenience import from new module within package

--- pylot/core/util/__init__.py
+++ pylot/core/util/__init__.py
@@ -1,6 +1,7 @@
 from pylot.core.util.connection import checkurl
 from pylot.core.util.defaults import FILTERDEFAULTS
 from pylot.core.util.errors import OptionsError
+from pylot.core.util.utils import fnConstructor
 from pylot.core.util.version import get_git_version as _getVersionString
 from pylot.core.util.widgets import (HelpForm,
                                      PickDlg,
