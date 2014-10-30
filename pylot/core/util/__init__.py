3b613d833078124b6c17185221c3957b6ba7c837
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2014-10-30 08:13:05 +0100

    see ticket #129 (future changes very likely)

--- pylot/core/util/__init__.py
+++ pylot/core/util/__init__.py
@@ -3,8 +3,4 @@ from pylot.core.util.defaults import FILTERDEFAULTS
 from pylot.core.util.errors import OptionsError
 from pylot.core.util.utils import fnConstructor
 from pylot.core.util.version import get_git_version as _getVersionString
-from pylot.core.util.widgets import (HelpForm,
-                                     PickDlg,
-                                     MPLWidget,
-                                     PropertiesDlg,
-                                     FilterOptionsDock)
+
