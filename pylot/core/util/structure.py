dbd53024b2979b8182b38094ea78d29aafd55728
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2015-01-26 21:08:07 +0100

    new module: make selection of either data structure easier

---
+++ pylot/core/util/structure.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 26 17:47:25 2015
+
+@author: sebastianw
+"""
+
+from pylot.core.read import SeiscompDataStructure, PilotDataStructure
+
+DATASTRUCTURE = {'PILOT':PilotDataStructure, 'SeisComP':SeiscompDataStructure}
