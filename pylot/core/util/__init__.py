3d8bea8f7eb634bdd940fffa756f9ed87d606c00
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2015-06-11 10:02:44 +0200

    deleted obsolete module

--- pylot/core/util/__init__.py
+++ pylot/core/util/__init__.py
@@ -1,7 +1,6 @@
 from pylot.core.util.connection import checkurl
 from pylot.core.util.defaults import FILTERDEFAULTS
 from pylot.core.util.errors import OptionsError, FormatError, DatastructureError
-from pylot.core.util.layouts import layoutStationButtons
 from pylot.core.util.utils import fnConstructor, createArrival, createEvent,\
     createPick, createAmplitude, createOrigin, createMagnitude, getOwner, \
     getHash, getLogin, createCreationInfo, createResourceID, prepTimeAxis, \
@@ -9,5 +8,3 @@ from pylot.core.util.utils import fnConstructor, createArrival, createEvent,\
 from pylot.core.util.widgets import PickDlg, HelpForm, FilterOptionsDialog,\
     PropertiesDlg, NewEventDlg, MPLWidget, createAction
 from pylot.core.util.version import get_git_version as _getVersionString
-
-
