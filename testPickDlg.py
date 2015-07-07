d53a3dad047d2201509fec00f3dffca5b3cc8c65
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2015-07-07 11:22:19 +0200

    make the test work again

--- testPickDlg.py
+++ testPickDlg.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
 import sys
+import matplotlib
+
+matplotlib.use('Qt4Agg')
+matplotlib.rcParams['backend.qt4'] = 'PySide'
+
 from PySide.QtGui import QApplication
 from obspy.core import read
 from pylot.core.util.widgets import PickDlg
+import icons_rc
 
 app = QApplication(sys.argv)
 
