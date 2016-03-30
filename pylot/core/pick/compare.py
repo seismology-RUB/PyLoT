76788c022382e2fa8efebf40c9904cf8d0b1e2c5
Sebastianw Wehling-Benatelli <sebastian.wehling@rub.de>
2016-03-30 11:45:49 +0200

    [refs #195] initial import of new interfaces module

---
+++ pylot/core/pick/compare.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pylot.core.util.pdf import ProbabilityDensityFunction
+from pylot.core.util.version import get_git_version as _getVersionString
+
+__version__ = _getVersionString()
+__author__ = 'sebastianw'
