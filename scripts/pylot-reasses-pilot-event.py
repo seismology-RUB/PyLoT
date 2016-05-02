8cb4f11bf804aa4944527bdee7dfd1ce70ebec4b
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2016-05-02 14:10:11 +0200

    [edit] implementing reassessment functions

--- scripts/pylot-reasses-pilot-event.py
+++ scripts/pylot-reasses-pilot-event.py
@@ -2,17 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+
+from pylot.core.pick.utils import reassess_pilot_event
 from pylot.core.util.version import get_git_version as _getVersionString
-from pylot.core.io.phases import reasses_pilot_event
 
 __version__ = _getVersionString()
 __author__ = 'sebastianw'
 
-def reassess_pilot_event():
-    pass
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--TSNR', type=tuple, help='length of time windows around pick used to determine SNR \
+        [s] (Tnoise, Tgap, Tsignal)', action=store_value
+    )
 
     args = parser.parse_args()
-    reasses_pilot_event(args.id)
+    reassess_pilot_event(args.db, args.id, args.TSNR)
