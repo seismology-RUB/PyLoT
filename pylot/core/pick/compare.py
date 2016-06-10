be326cba7b5130c10b87884063c0578acdae52a8
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2016-06-10 09:03:16 +0200

    [bugfix] automatic data with the S-P time saved in the pickdict caused Exception in the ProbabilityDensityFunction constructor

--- pylot/core/pick/compare.py
+++ pylot/core/pick/compare.py
@@ -270,6 +270,8 @@ class PDFDictionary(object):
 
         for station, phases in pdf_picks.items():
             for phase, values in phases.items():
+                if phase not in 'PS':
+                    continue
                 phases[phase] = ProbabilityDensityFunction.from_pick(
                     values['epp'],
                     values['mpp'],
