11a3839cff6bd2468ffc76bbb376d6618fad7e18
Ludger Küperkoch <kueperkoch@bestec-for-nature.com>
2017-02-28 14:56:09 +0100

    Additional phase output formated for HYPOSAT.

---
+++ pylot/core/loc/hyposat.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pylot.core.io.phases import writephases
+from pylot.core.util.version import get_git_version as _getVersionString
+
+__version__ = _getVersionString()
+
+def export(picks, fnout, parameter):
+    '''
+    Take <picks> dictionary and exports picking data to a HYPOSAT
+    <phasefile> without creating an ObsPy event object.
+
+    :param picks: picking data dictionary
+    :type picks: dict
+
+    :param fnout: complete path to the exporting obs file
+    :type fnout: str
+    
+    :param: parameter, all input information
+    :type:  object
+    '''
+    # write phases to NLLoc-phase file
+    writephases(picks, 'HYPOSAT', fnout, parameter)
