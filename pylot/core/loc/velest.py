44d2acf331abda34c2261562f7540e48bb11a717
Ludger Küperkoch <kueperkoch@bestec-for-nature.com>
2017-03-02 09:19:11 +0100

    Output formated for further VELEST processing.

--- pylot/core/loc/velest.py
+++ pylot/core/loc/velest.py
@@ -1,2 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from pylot.core.io.phases import writephases
+from pylot.core.util.version import get_git_version as _getVersionString
+
+__version__ = _getVersionString()
+
+def export(picks, fnout, parameter, eventinfo):
+    '''
+    Take <picks> dictionary and exports picking data to a VELEST-cnv
+    <phasefile> without creating an ObsPy event object.
+
+    :param picks: picking data dictionary
+    :type picks: dict
+
+    :param fnout: complete path to the exporting obs file
+    :type fnout: str
+    
+    :param: parameter, all input information
+    :type:  object
+
+    :param: eventinfo, optional, source time needed for VELEST-cnv format
+    :type:  list object
+    '''
+    # write phases to NLLoc-phase file
+    writephases(picks, 'VELEST', fnout, parameter, eventinfo)
