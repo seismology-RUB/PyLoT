ef1755a80f3b8ad08fddfd741b3cb25acb6bfcf1
Marcel Paffrath <marcel.paffrath@rub.de>
2017-04-06 13:17:06 +0200

    added new files as well

---
+++ pylot/core/loc/hash.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pylot.core.io.phases import writephases
+from pylot.core.util.version import get_git_version as _getVersionString
+
+__version__ = _getVersionString()
+
+def export(picks, fnout, parameter, eventinfo):
+    '''
+    Take <picks> dictionary and exports picking data to a HASH
+    <phasefile> without creating an ObsPy event object.
+
+    :param picks: picking data dictionary
+    :type picks: dict
+
+    :param fnout: complete path to the exporting obs file
+    :type fnout: str
+    
+    :param: parameter, all input information
+    :type:  object
+
+    :param: eventinfo, source information needed for HASH format
+    :type:  list object
+    '''
+    # write phases to HASH-phase file
+    writephases(picks, 'HASH', fnout, parameter, eventinfo)
