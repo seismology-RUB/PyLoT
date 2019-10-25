0ef2afa775825c4bc3b8db8def85bff68e54b179
Ludger Küperkoch <kueperkoch@bestec-for-nature.com>
2019-10-25 16:20:48 +0200

    Update of event object for moment magntiude was missing.

--- PyLoT.py
+++ PyLoT.py
@@ -3386,7 +3386,8 @@ class MainWindow(QMainWindow):
         # calculate moment magnitude
         moment_mag = MomentMagnitude(corr_wf, self.get_data().get_evt_data(), self.inputs.get('vp'),
                                      self.inputs.get('Qp'), self.inputs.get('rho'), verbosity=True)
-        event_moment_mag = moment_mag.net_magnitude()
+        moment_mag = moment_mag.net_magnitude()
+        event_moment_mag = moment_mag.updated_event()
         if event_moment_mag is not None:
             print("Network moment magnitude: %4.1f" % event_moment_mag.mag)
 
