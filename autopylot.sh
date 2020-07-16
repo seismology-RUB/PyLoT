32c95757c6692ba830e4ab68dec7f463b64ba1b0
Marcel <marcel.paffrath@rub.de>
2020-07-16 12:36:23 +0200

    [minor] small changes to autopylot.sh script

--- autopylot.sh
+++ autopylot.sh
@@ -1,1 +1,10 @@
-python ./autoPyLoT.py -i /home/marcel/.pylot/pylot_alparray_mantle_m6.0-6.4.in -dmt processed -c $NSLOTS
+#!/bin/bash
+
+#$ -l low
+#$ -cwd
+#$ -pe smp 40
+#$ -l mem=2G
+#$ -l h_vmem=2G
+#$ -l os=*stretch
+
+python ./autoPyLoT.py -i /home/marcel/.pylot/pylot_alparray_mantle_corr_stack_0.03-0.5.in -dmt processed -c $NSLOTS
