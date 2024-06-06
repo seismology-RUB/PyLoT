c79e886d77dceffb86342cabd5fef6cad8220deb
Marcel <marcel.paffrath@rub.de>
2024-06-06 15:55:47 +0200

    [minor] update default shell script for SGE

--- autopylot.sh
+++ autopylot.sh
@@ -3,10 +3,10 @@
 #$ -l low
 #$ -cwd
 #$ -pe smp 40
-#$ -l mem=2G
-#$ -l h_vmem=2G
+##$ -l mem=3G
+#$ -l h_vmem=6G
 #$ -l os=*stretch
 
-conda activate pylot_38
+conda activate pylot_311
 
 python ./autoPyLoT.py -i /home/marcel/.pylot/pylot_adriaarray.in -c 20 -dmt processed
