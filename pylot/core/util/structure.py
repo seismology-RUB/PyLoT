e5c7404bb6b5a1ad5f69ee7738b462e849cd2bb7
Sebastian Wehling-Benatelli <sebastian.wehling@rub.de>
2024-07-17 14:07:05 +0200

    refactor: remove unnecessary additional declaration

--- pylot/core/util/structure.py
+++ pylot/core/util/structure.py
@@ -6,7 +6,7 @@ Created on Wed Jan 26 17:47:25 2015
 @author: sebastianw
 """
 
-from pylot.core.io.data import SeiscompDataStructure, PilotDataStructure, ObspyDMTdataStructure
+from pylot.core.io.data import SeiscompDataStructure, PilotDataStructure
 
 DATASTRUCTURE = {'PILOT': PilotDataStructure, 'SeisComP': SeiscompDataStructure,
-                 'obspyDMT': ObspyDMTdataStructure, None: PilotDataStructure}
+                 'obspyDMT': PilotDataStructure, None: PilotDataStructure}
